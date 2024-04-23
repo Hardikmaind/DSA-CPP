0eb92d2a69424490fcae77faa7192697c7185e5f
Hardikmaind <maindhardik@gmail.com>
2024-04-23 21:45:15 +0530

    code added

---
+++ Codechef/_cc_S130_UpdatingA.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        int a[n],p[n+1];p[0]=0;
+        int s=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];s+=a[i];
+            p[i+1]=p[i]+a[i];
+        }
+        int mx=0,pmx=0;
+        for(int i=2;i<n+1;i++){
+            mx=max(mx,pmx-p[i]);
+            pmx=max(pmx,p[i-1]);
+        }
+        cout<<s+2*mx<<"\n";
+        
+    }
+}
