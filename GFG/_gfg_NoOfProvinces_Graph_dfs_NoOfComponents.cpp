1909e8917bf5121c55c5955718715f0ef90a0913
Hardikmaind <maindhardik@gmail.com>
2025-04-18 20:21:08 +0530

    _200_NumberOfIslands_bfs_dfs_Graphs

--- GFG/_gfg_NoOfProvinces_Graph_dfs_NoOfComponents.cpp
+++ GFG/_gfg_NoOfProvinces_Graph_dfs_NoOfComponents.cpp
@@ -32,6 +32,37 @@ public:
         return noOfProvince;
     }
 };
+class Solution2
+{
+public:
+    void dfs(int src, vector<int> &vis, vector<vector<int>> &adj)
+    {
+        vis[src] = 1;
+        for (int i = 0; i < adj.size(); i++)
+        {
+            if (adj[src][i] && !vis[i])
+            {
+                dfs(i, vis, adj);
+            }
+        }
+    }
+    int numProvinces(vector<vector<int>> adj, int V)
+    {
+        // code here
+        int noOfProvince = 0;
+        vector<int> vis(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+
+                noOfProvince++;
+                dfs(i, vis, adj);
+            }
+        }
+        return noOfProvince;
+    }
+};
 
 int main()
 {
