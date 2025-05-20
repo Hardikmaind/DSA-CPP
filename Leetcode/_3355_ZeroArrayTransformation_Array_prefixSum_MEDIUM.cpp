2f31313c151229b56c9f742962fdcffa421002f7
Hardikmaind <maindhardik@gmail.com>
2025-05-20 21:12:07 +0530

    _gfg_BurningTree_Bfs_Tree_HARD _3355_ZeroArrayTransformation_Array_prefixSum_MEDIUM

---
+++ Leetcode/_3355_ZeroArrayTransformation_Array_prefixSum_MEDIUM.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> deltaArray(nums.size() + 1, 0);
+        for (const auto& query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }
+        vector<int> operationCounts;
+        int currentOperations = 0;
+        for (int delta : deltaArray) {
+            currentOperations += delta;
+            operationCounts.push_back(currentOperations);
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution solution;
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> queries = {{0, 1}, {1, 2}};
+    bool result = solution.isZeroArray(nums, queries);
+    cout << (result ? "true" : "false") << endl; // Output: false
+    return 0;
+}
