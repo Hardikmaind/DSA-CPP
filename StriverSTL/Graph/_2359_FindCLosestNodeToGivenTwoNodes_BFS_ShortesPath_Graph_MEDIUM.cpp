8d65be10a67d0a1f349ceab98e1f911baf615562
Hardikmaind <maindhardik@gmail.com>
2025-05-31 00:29:13 +0530

    _2359_FindCLosestNodeToGivenTwoNodes_BFS_ShortesPath_Graph_MEDIUM ShortestPathWithBfs

---
+++ StriverSTL/Graph/_2359_FindCLosestNodeToGivenTwoNodes_BFS_ShortesPath_Graph_MEDIUM.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void bfs(int startNode, vector<int>& edges, vector<int>& dist) {
+        int n = edges.size();
+        queue<int> q;
+        q.push(startNode);
+
+        vector<bool> visit(n);
+        dist[startNode] = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            if (visit[node]) {
+                continue;
+            }
+
+            visit[node] = true;
+            int neighbor = edges[node];
+            if (neighbor != -1 && !visit[neighbor]) {
+                dist[neighbor] = 1 + dist[node];
+                q.push(neighbor);
+            }
+        }
+    }
+
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<int> dist1(n, numeric_limits<int>::max()), dist2(n, numeric_limits<int>::max());
+
+        bfs(node1, edges, dist1);
+        bfs(node2, edges, dist2);
+
+        int minDistNode = -1, minDistTillNow = numeric_limits<int>::max();
+        for (int currNode = 0; currNode < n; currNode++) {
+            if (minDistTillNow > max(dist1[currNode], dist2[currNode])) {
+                minDistNode = currNode;
+                minDistTillNow = max(dist1[currNode], dist2[currNode]);
+            }
+        }
+
+        return minDistNode;
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int> edges = {2, 2, 3, -1};
+    int node1 = 0, node2 = 1;
+    int result = obj.closestMeetingNode(edges, node1, node2);
+    cout << "Closest meeting node: " << result << endl; // Output: 2
+    return 0;
+}
