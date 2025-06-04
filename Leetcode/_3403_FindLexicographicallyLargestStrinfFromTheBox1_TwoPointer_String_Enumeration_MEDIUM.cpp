bfc515ea299972048d7350e3db3e5680df100150
Hardikmaind <maindhardik@gmail.com>
2025-06-05 00:03:29 +0530

    _3403_FindLexicographicallyLargestStrinfFromTheBox1_TwoPointer_String_Enumeration_MEDIUM

---
+++ Leetcode/_3403_FindLexicographicallyLargestStrinfFromTheBox1_TwoPointer_String_Enumeration_MEDIUM.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string lastSubstring(string s) {
+        int i = 0, j = 1, n = s.size();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s[i + k] == s[j + k]) {
+                k++;
+            }
+            if (j + k < n && s[i + k] < s[j + k]) {
+                int t = i;
+                i = j;
+                j = max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substr(i, n - i);
+    }
+
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        string last = lastSubstring(word);
+        int n = word.size(), m = last.size();
+        return last.substr(0, min(m, n - numFriends + 1));
+    }
+};
+
+
+int main(){
+    Solution a;
+    string word = "dbca";
+    int numFriends = 2;
+    string res = a.answerString(word, numFriends);
+    cout << res << endl; // Output: "ab"
+    return 0;
+}
