5e76483f70eb1b998a2b816885909b0bcc1c990a
Hardikmaind <maindhardik@gmail.com>
2025-07-19 22:20:46 +0530

    _gfg_CountUniqueVowelStrings_Array_Strings_Combinatorics_MEDIUM _gfg_LCMTriangle_Math_MEDIUM _1233_RemovesSubFoldersFromTheFilesystem_Array_Trie_Sorting_DFS_MEDIUM _2163_MinimumDifferenceInSumsAfterRemovalOfElements_DP_Array_PriorityQueue_Heap_HARD

---
+++ GFG/_gfg_LCMTriangle_Math_MEDIUM.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int hcf_fun(int a, int b){
+        if(b==0)
+          return a;
+        return hcf_fun(b, a%b);
+    }
+    
+    int lcm_fun(int a, int b){
+        return (a*b)/hcf_fun(a, b);
+    }
+    
+    int lcmTriplets(int n) {
+        // code here
+        if(n<3)
+          return n;
+        
+        if(n&1)
+          return lcm_fun(n, lcm_fun(n-1, n-2));
+        return max(lcm_fun(n, lcm_fun(n-1, n-3)), lcm_fun(n-1, lcm_fun(n-2, n-3))); 
+    }
+};
+
+int main(){
+    Solution ob;
+    int n=5; // Example input
+    cout << ob.lcmTriplets(n) << endl; // Output the result
+    return 0;
+}
---
+++ GFG/_gfg_PowerOFkInFactorialOfN_Factorial_math_MEDIUM.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+Class Solution{
+    public:
+        int maxKPower(int &n,int&k){
+            vector<int> primes;
+            vector<bool> isPrime(n + 1, true);
+        }
+    
+};
