6b69f7cc9e0edc64fb12519a5ebca07e51200a50
Hardikmaind <maindhardik@gmail.com>
2025-07-29 00:03:13 +0530

    _2044_CountNumberOfMaximumBitwiseORSubsets_Array_Backtracking_Enumeration_Bitmanipulation_MEDIUM _gfg_MakeMatrixBeautiful_Matix_MEDIUM.cpp

---
+++ GFG/_gfg_MakeMatrixBeautiful_Matix_MEDIUM.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int balanceSums(vector<vector<int>>& mat) {
+
+        int n = mat.size();
+        int row_sum = 0, col_sum = 0;
+        int max_sum = 0, total = 0;
+
+        for (int i = 0; i < n; i++) {
+            row_sum = 0;
+            col_sum = 0;
+            for (int j = 0; j < n; j++) {
+                row_sum += mat[i][j];
+                col_sum += mat[j][i];
+            }
+            max_sum = max({max_sum, row_sum, col_sum});
+            total += row_sum;
+        }
+        
+        return (n * max_sum) - total; 
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<vector<int>> mat = {{1, 2}, {3, 4}};
+    cout << sol.balanceSums(mat) << endl; // Output: 2
+    return 0;
+}
